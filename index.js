62cdc9897f9143533344adfd90b4dbf693409a27
Arvindchocha <109568457+Arvindchocha@users.noreply.github.com>
2022-11-03 12:26:26 +0530

    added

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-// const hello = "Hello World!";
-// console.log(hello);
+
+
+//synchronus(blocking)
 
 const fs = require('fs');
-const textIn = fs.readFileSync('txtinput.txt','utf-8');
-console.log(textIn);
+// const textIn = fs.readFileSync('txtinput.txt','utf-8');
+// console.log(textIn);
+
+const txtdata = "this is writing method of node";
+fs.writeFileSync('txtinput.txt',txtdata);
+console.log('File Written..!');
+
+//Async(Non-blocking)
+// const fs = require('fs');
+// const textIn = fs.readFile('txtinput.txt','utf-8',(err,data)=>{
+//     console.log(data);
+//     // console.log(err);
+// });
+// console.log("Reading file...");
